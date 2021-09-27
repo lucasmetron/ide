05ece5aa041207943df300352cbad32df07f315d
Lucas Rosa <lucasmetron@gmail.com>
2021-09-27 19:51:14 -0300

    ajsutes padding

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import Hamburguer from './components/Hamburguer/Hamburguer';
 import Header from './components/Header/Header';
+import videoMob from './assets/videos/site_fundo.mp4'
 
 function App() {
   return (
     <div className="containerGeral">
       <Header />
       <Hamburguer />
+
+      {/* <video className='videoBackground' autoplay loop >
+        <source src={videoMob} type='video/mp4' />
+      </video> */}
     </div>
   );
 }
